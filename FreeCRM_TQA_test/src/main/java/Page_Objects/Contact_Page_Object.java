51e75d83b4d7b854976ee6eb150078d861266457
user 1 <user 1@DESKTOP-NQMJ256>
2022-06-21 18:34:07 +0530

    Commit 3

--- FreeCRM_TQA_test/src/main/java/Page_Objects/Contact_Page_Object.java
+++ FreeCRM_TQA_test/src/main/java/Page_Objects/Contact_Page_Object.java
@@ -5,14 +5,20 @@ import java.io.IOException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
 
 import Test_Base.Base_class;
 
 public class Contact_Page_Object extends Base_class{
 	
 	
-
-
+	@FindBy(xpath="//span[contains(text(),'Contacts')]")
+	WebElement Contact_Tab;
+    
+	@FindBy(xpath="//div[@id='main-content']/div/div[1]/div[2]/div/a/button")
+	WebElement Create_Button;
+    
+	
 	@FindBy(xpath="//span[contains(text(),'The field First Name is required')]")
 	WebElement First_name_mandatory_message;
 	
@@ -40,6 +46,11 @@ public class Contact_Page_Object extends Base_class{
 	@FindBy(xpath="//div[@name='status']/span[contains(text(),'New')]")
 	WebElement New_status;
 	
+	@FindBy(xpath="//span[contains(text(),'Home')]")
+	WebElement Home_tab;
+	
+	@FindBy(xpath="//a[contains(text(),'Akas test')]")
+	WebElement Verify_Contact_Created;
 	
 	public Contact_Page_Object() throws IOException {
 		PageFactory.initElements(driver, this);
@@ -47,6 +58,43 @@ public class Contact_Page_Object extends Base_class{
 	}
 	
 	
+	public void Mandatory_Check() throws InterruptedException {
+		Contact_Tab.click();
+		Thread.sleep(4000);
+		Create_Button.click();
+		Thread.sleep(4000);
+		Save_button.click();
+		Thread.sleep(3000);
+		boolean Firstname_mandatory = First_name_mandatory_message.isDisplayed();
+		Assert.assertEquals(Firstname_mandatory, true);
+		System.out.println("Firstname_Mandatory:---" + Firstname_mandatory);
+		boolean Lastname_mandatory = Last_name_mandatory_message.isDisplayed();
+		Assert.assertEquals(Lastname_mandatory, true);
+		System.out.println("Firstname_Mandatory:---" + Lastname_mandatory);
+		
+		
+	}
+	
+	public void Create_Contact() throws InterruptedException {
+		Contact_Tab.click();
+		Thread.sleep(4000);
+		Create_Button.click();
+		First_name.sendKeys("Akash");
+		Last_name.sendKeys("Saha");
+		Status.click();
+		New_status.click();
+		Thread.sleep(3000);
+		Save_button.click();
+		Thread.sleep(3000);
+	}
+	
+	public void Verify_Contact() {
+		Home_tab.click();
+		boolean Contact_Creation = First_name_mandatory_message.isDisplayed();
+		Assert.assertEquals(Contact_Creation, true);
+		System.out.println("Firstname_Mandatory:---" + Contact_Creation);
+		
+	}
 	
 	
 	
---
+++ FreeCRM_TQA_test/src/test/java/Test_case/Test_Case_Contact.java
@@ -0,0 +1,57 @@
+package Test_case;
+
+import java.io.IOException;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import Page_Objects.Contact_Page_Object;
+import Page_Objects.Home_Page_Object;
+import Page_Objects.Login_Logout_page_object;
+import Test_Base.Base_class;
+
+public class Test_Case_Contact extends Base_class {
+	public Login_Logout_page_object Login;
+	   public Contact_Page_Object Contact_page_tc;
+	public Test_Case_Contact() throws IOException {
+		super();
+
+	}
+	@BeforeMethod
+	public void intialization_contact_page() throws InterruptedException, IOException {
+		Initialization();
+		Thread.sleep(4000);
+		Login = new Login_Logout_page_object();
+		Login.login();
+		Contact_page_tc = new Contact_Page_Object();
+		Thread.sleep(4000);
+		
+	}
+	 @Test(priority = 1)
+	 public void TC_1() throws InterruptedException {
+     
+		 Contact_page_tc.Mandatory_Check();
+	 }
+	
+	 @Test(priority = 2)
+	 public void TC_2() throws InterruptedException {
+     
+		 Contact_page_tc.Create_Contact();
+	
+	 }
+	 @Test(priority = 3)
+	 public void TC_3() throws InterruptedException {
+	     
+		 Contact_page_tc.Verify_Contact();
+	 
+	 
+
+     }
+	 
+	 @AfterMethod
+	 public void tear_down() {
+		 driver.close();
+	 }
+	 
+}	 
