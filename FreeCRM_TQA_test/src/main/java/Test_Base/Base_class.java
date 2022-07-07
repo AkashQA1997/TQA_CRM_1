37a39889b7099c46affb7c55628531d53a09d988
user 1 <user 1@DESKTOP-NQMJ256>
2022-07-07 15:25:58 +0530

    Merge branch 'Akash_Branch' of https://github.com/AkashQA1997/TQA_CRM_1.git into Akash_Branch

--- FreeCRM_TQA_test/src/main/java/Test_Base/Base_class.java
+++ FreeCRM_TQA_test/src/main/java/Test_Base/Base_class.java
@@ -27,7 +27,7 @@ public class Base_class {
 		prop = new Properties();
 		FileInputStream file;
 		try {
-			file = new FileInputStream("C:\\Users\\user 1\\git\\repository3\\TQA_CRM_1\\FreeCRM_TQA_test\\Cofig_Folder\\config.properties");
+			file = new FileInputStream("C:\\Users\\user 1\\git\\repository3\\FreeCRM_TQA_test\\Cofig_Folder\\config.properties");
 			prop.load(file);
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
